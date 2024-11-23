ae3c3ac12ed7d36b6e02f67491cc856e36887a2e
Aaron <aaron.m@hyperverge.co>
2024-11-24 01:22:33 +0530

    frontend commit

--- src/Components/bottomNav.js
+++ src/Components/bottomNav.js
@@ -4,18 +4,18 @@ import { NavLink } from 'react-router-dom';
 
 const BottomNav = () => {
   return (
-    <nav class="fixed bottom-0 left-0 w-full h-[60px] bg-[#FDF8FA] flex justify-around items-center border-t border-gray-300 z-1000 no-border rounded-t-3xl">
+    <nav class="fixed bottom-0 left-0 w-full h-[60px] bg-black flex justify-around items-center border-t border-gray-300 z-1000 no-border rounded-t-3xl">
       <NavLink to="/" className=" flex flex-col  items-center">
-        <FaHome className="text-[16px] text-black"/>
-        <span className="block mt-1 text-black text-[12px]">Home</span>
+        <FaHome className="text-[16px] text-white"/>
+        <span className="block mt-1 text-white text-[12px]">Home</span>
       </NavLink>
       <NavLink to="/list" className=" flex flex-col items-center " >
-        <FaList className="text-[16px] text-black"/>
-        <span className="block mt-1 text-black text-[12px]">List</span>
+        <FaList className="text-[16px] text-white"/>
+        <span className="block mt-1 text-white text-[12px]">List</span>
       </NavLink>
       <NavLink to="/roster" className=" flex flex-col  items-center" >
-        <FaRegEdit className="text-[16px] text-black"/>
-        <span className="block mt-1 text-black text-[12px]">Roster</span>
+        <FaRegEdit className="text-[16px] text-white"/>
+        <span className="block mt-1 text-white text-[12px]">Roster</span>
       </NavLink>
     </nav>
   );
