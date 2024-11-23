38d1066cac9d4b497be632eff1098b1579cba638
Aaron <aaron.m@hyperverge.co>
2024-11-23 23:36:32 +0530

    followup

--- src/pages/songs.js
+++ src/pages/songs.js
@@ -35,7 +35,7 @@ const Songs = () => {
     <div className='bg-gradient-to-b from-[#DDD5F5] to-[#FFFFFF] h-screen w-full'>
       <h1 className='text-2xl font-bold pt-10 pl-10 text-left mb-6'>This Weeks Songs</h1>
       {songs.map((song, index)=>(
-        <div className='text-left px-10 flex justify-between items-center font-semibold truncate'>{index+1}. {song.songName}
+        <div className='text-left px-10 flex justify-between items-center font-semibold truncate mb-3'>{index+1}. {song.songName}
               <button className='bg-white px-4 py-3 rounded text-left' onClick={()=> openLink(song.link)}>Chord Sheet</button>
 </div>
         
